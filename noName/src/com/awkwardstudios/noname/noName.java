1fd6adfe455d061f9e4af8f7c549611245d08967
SupaStuff <StuffQilla@gmail.com>
2014-01-14 21:54:21 -0500

    Empty project attempt #n

--- noName/src/com/awkwardstudios/noname/noName.java
+++ noName/src/com/awkwardstudios/noname/noName.java
@@ -11,81 +11,81 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class noName implements ApplicationListener {
-	/*
-	 * give noName some properties
-	 * 
-	private OrthographicCamera camera;
-	private SpriteBatch batch;
-	private Texture texture;
-	private Sprite sprite;
-	*/
-	
-	@Override
-	public void create() {		
-		/*
-		 * initialize the properties here
-		 * 
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
-		
-		camera = new OrthographicCamera(1, h/w);
-		batch = new SpriteBatch();
-		
-		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
-		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
-		*/
-	}
+        /*
+         * give noName some properties
+         * 
+        private OrthographicCamera camera;
+        private SpriteBatch batch;
+        private Texture texture;
+        private Sprite sprite;
+        */
+        
+        @Override
+        public void create() {                
+                /*
+                 * initialize the properties here
+                 * 
+                float w = Gdx.graphics.getWidth();
+                float h = Gdx.graphics.getHeight();
+                
+                camera = new OrthographicCamera(1, h/w);
+                batch = new SpriteBatch();
+                
+                texture = new Texture(Gdx.files.internal("data/libgdx.png"));
+                texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+                
+                TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
+                
+                sprite = new Sprite(region);
+                sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
+                sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
+                sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+                */
+        }
 
 
-	@Override
-	public void dispose() {
-		/*
-		 *delete stuff here
-		 * 
-		batch.dispose();
-		texture.dispose();
-		*/
-	}
+        @Override
+        public void dispose() {
+                /*
+                 *delete stuff here
+                 * 
+                batch.dispose();
+                texture.dispose();
+                */
+        }
 
-	@Override
-	public void render() {		
-		/*
-		 * every frame
-		 * 
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		sprite.draw(batch);
-		batch.end();
-		*/
-	}
+        @Override
+        public void render() {                
+                /*
+                 * every frame
+                 * 
+                Gdx.gl.glClearColor(1, 1, 1, 1);
+                Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+                
+                batch.setProjectionMatrix(camera.combined);
+                batch.begin();
+                sprite.draw(batch);
+                batch.end();
+                */
+        }
 
-	@Override
-	public void resize(int width, int height) {
-	}
+        @Override
+        public void resize(int width, int height) {
+        }
 
-	@Override
-	public void pause() {
-	}
+        @Override
+        public void pause() {
+        }
 
-	@Override
-	public void resume() {
-	}
-	
+        @Override
+        public void resume() {
+        }
+        
 
-	public static void loggi(String s)
-	{
-		//For debugging shit.
-		//In case we are too lazy, we an just comment this line out before release
-		System.out.printf(s+"\n");
-	}
+        public static void loggi(String s)
+        {
+                //For debugging shit.
+                //In case we are too lazy, we an just comment this line out before release
+                System.out.printf(s+"\n");
+        }
 }
